ad32ccaf1a2b60f23561dee0e2f4baf7fcc7944d
soubj <sobujahmed124@gmail.com>
2023-05-19 20:31:48 +0600

    made the navbar sticky-top

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -8,27 +8,24 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <div
-      className="position-relative"
       style={{
         backgroundImage: `url("https://img.freepik.com/premium-photo/closeup-muslim-father-passing-his-son-lafah-bread-during-dinner-dining-table-ramadan-ai-generative_337384-4780.jpg?w=740") `,
         backgroundSize: "cover",
         height: "100vh",
         backgroundRepeat: "no-repeat",
       }}>
-      <Navbar
-        className="bg-primary bg-opacity-75 position-sticky"
-        variant="dark">
+      <Navbar className="bg-primary bg-opacity-75 sticky-top" variant="dark">
         <Container>
           <Navbar.Brand className="me-6">Chefs Choice</Navbar.Brand>
           <div className="mx-auto ">
             <NavLink
-              className={` me-5 text-decoration-none  ${(isActive) =>
+              className={`me-3 md-me-5 text-decoration-none  ${(isActive) =>
                 isActive ? "active" : "text-white"}`}
               to="/">
               Home
             </NavLink>
             <NavLink
-              className={` me-5 text-decoration-none ${(isActive) =>
+              className={`me-2 md-me-5 text-decoration-none ${(isActive) =>
                 isActive ? "active" : " text-white"}`}
               to="/blog">
               Blog
@@ -39,8 +36,8 @@ const NavBar = () => {
           </Link>
         </Container>
       </Navbar>
-      <div className="hero-text">
-        <h2 className="text-white">Arabian Cuisine</h2>
+      <div className="hero-text d-flex align-items-center justify-content-center">
+        <h1 className="text-white ">Arabian Cuisine</h1>
       </div>
     </div>
   );
