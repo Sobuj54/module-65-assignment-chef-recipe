642ea8a5c8e8ab3049516d4166e36f6578bfe18b
soubj <sobujahmed124@gmail.com>
2023-05-21 22:37:31 +0600

    showed chef details in cards and made cards responsive

--- src/components/chef/Chef.jsx
+++ src/components/chef/Chef.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
 import ChefDetails from "../chefDetails/ChefDetails";
+import "./Chef.css";
 
 const Chef = () => {
   const chefs = useLoaderData();
 
   return (
-    <Container>
+    <div className="chefInfo">
       {chefs.map((chef) => (
         <ChefDetails key={chef.id} chef={chef}></ChefDetails>
       ))}
-    </Container>
+    </div>
   );
 };
 
--- src/components/chefDetails/ChefDetails.jsx
+++ src/components/chefDetails/ChefDetails.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./chefDetails.css";
 
 const ChefDetails = ({ chef }) => {
   const { name, picture, yearsOfExperience, numRecipes, likes } = chef;
   return (
-    <div>
+    <div className="chefDetails">
       <h4>{name}</h4>
-      <img src={picture} className="rounded" />
+      <img src={picture} className="rounded image" />
       <p>Experience: {yearsOfExperience} Years</p>
       <p>Recipes: {numRecipes}</p>
       <p>Likes: {likes}</p>
